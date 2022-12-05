fb8f07e2fdee1d7abc67dbca67a9098189b78f3f
Cássio Pereira <cassiorodp@gmail.com>
2022-12-05 09:57:52 -0300

    fix: use es6 modules

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,19 +1,17 @@
-namespace App {
-  // AutoBind Decorator
-  export function AutoBind(
-    _target: any,
-    _methodName: string,
-    descriptor: PropertyDescriptor
-  ) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
+// AutoBind Decorator
+export function AutoBind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
 
-    return adjDescriptor;
-  }
+  return adjDescriptor;
 }
